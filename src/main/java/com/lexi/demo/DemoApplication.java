07af2eb78c7e3da49b4d365b13afadd788500ee2
ian lombu <citrakemurnian@gmail.com>
2021-02-23 09:12:28 +0700

    First Commit

---
+++ src/main/java/com/lexi/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.lexi.demo;
+
+import com.lexi.demo.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Ian Lombu",
+						"ian@gmail.com",
+						LocalDate.of(2021, Month.FEBRUARY, 21),
+						21
+				)
+		);
+	}
+
+}
